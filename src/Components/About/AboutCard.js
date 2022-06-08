053859dfaffb09555bb17d64275a3f0145a6b69b
HaiTrTh <haithanhtran00@gmail.com>
2022-06-08 23:21:11 +0700

    filter all file

---
+++ src/Components/About/AboutCard.js
@@ -0,0 +1,32 @@
+import React from "react"
+import "./About.css"
+
+const AboutCard = () => {
+  return (
+    <>
+      <div className='aboutCard mtop flex_space'>
+        <div className='row row1'>
+          <h4>About Us</h4>
+          <h1>
+            We <span>provide Solution</span> to grow your business
+          </h1>
+          <p>Since opening its doors in 2001, The Peninsula Chicago has been awarded numerous accolades including prestigious Forbes Five Star and AAA Five Diamond awards every year.</p>
+          <p>Kindly check the terms and conditions at the time of booking or consult with our reservations agent or your preferred travel advisor.</p>
+          <button className='secondary-btn'>
+            Explore More <i className='fas fa-long-arrow-alt-right'></i>
+          </button>
+        </div>
+        <div className='row image'>
+          <img style={{ width: 700 }} src='/images/about-img-1.jpg' alt='' />
+          <div className='control-btn'>
+            <button className='prev'>
+              <i className='fas fa-play'></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default AboutCard
