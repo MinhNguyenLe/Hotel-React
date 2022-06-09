b383d48c03ea98b586bf71fa5c509c7db680ee79
HaiTrTh <haithanhtran00@gmail.com>
2022-06-09 22:59:32 +0700

    done files

---
+++ src/Components/HomeSection/Works/Wdata.js
@@ -0,0 +1,21 @@
+const Wdata = [
+  {
+    id: 1,
+    cover: "/images/works/work-1.png",
+    title: "Search Multiple Destinations",
+    desc: "Sitting atop the 18th floor over Michigan and Chicago Avenues, The Peninsula Suite has been newly refreshed with a contemporary and elegant look that celebrates modern",
+  },
+  {
+    id: 1,
+    cover: "/images/works/work-3.png",
+    title: "Find the Lowest Hotel Prices",
+    desc: " while preserving timeless artwork and touches of heritage. Expect every amenity, from a baby grand piano to wrap-around terrace overlooking Lake Michigan.",
+  },
+  {
+    id: 1,
+    cover: "/images/works/work-2.png",
+    title: "FOR EXCLUSIVE RESERVATIONS PLEASE SEND US AN ENQUIRY.",
+    desc: "Flooded with natural light, The Peninsula Suite epitomizes the fresh, bright feel of the entire hotel.",
+  },
+]
+export default Wdata
